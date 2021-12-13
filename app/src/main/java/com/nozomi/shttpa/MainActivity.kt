8d12f72065fc4fbb859cf88b31b36009a0e19733
nozomi hiragi(ひいらぎのぞみ) <nozomi.hiragi@gmail.com>
2021-12-13 20:03:22 +0900

    サーバーのスイッチ追加

--- app/src/main/java/com/nozomi/shttpa/MainActivity.kt
+++ app/src/main/java/com/nozomi/shttpa/MainActivity.kt
@@ -3,51 +3,73 @@ package com.nozomi.shttpa
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.nozomi.shttpa.ui.theme.ShttpaTheme
+import kotlinx.coroutines.launch
 import java.io.IOException
 
 class MainActivity : ComponentActivity() {
-    private var webServer: WebServer? = null
+    private lateinit var webServer: WebServer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.webServer = WebServer(applicationContext, 8080)
+
         setContent {
             ShttpaTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Home(webServer = this.webServer)
                 }
             }
-        }
-
-        this.webServer = WebServer(applicationContext, 8888)
-        try {
-            this.webServer?.start()
-        } catch (e: IOException) {
-            e.printStackTrace()
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        this.webServer?.stop()
+        this.webServer.stop()
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun Home(webServer: WebServer) {
+    val scaffoldState = rememberScaffoldState()
+    val snackbarCoroutineScope = rememberCoroutineScope()
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ShttpaTheme {
-        Greeting("Android")
+    Scaffold(scaffoldState = scaffoldState) {
+        Column(Modifier.padding(8.dp)) {
+            Row(Modifier.padding(16.dp)) {
+                var serverState by remember { mutableStateOf(webServer.wasStarted()) }
+
+                Text("Serve", style = MaterialTheme.typography.subtitle1)
+                Spacer(Modifier.weight(1f))
+                Switch(
+                    checked = serverState,
+                    onCheckedChange = {
+                        if (serverState) {
+                            webServer.stop()
+                        } else {
+                            try {
+                                webServer.start()
+                            } catch (e: IOException) {
+                                snackbarCoroutineScope.launch {
+                                    scaffoldState.snackbarHostState.showSnackbar(e.message.toString())
+                                }
+                                e.printStackTrace()
+                            }
+                        }
+                        serverState = webServer.isAlive
+                    },
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
+        }
     }
 }
